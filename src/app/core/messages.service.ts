82a128557ed4dbbbafde210010f522fc67b9702a
ashwinlagji <ashwin_lagji@persistent.co.in>
2017-04-13 17:54:15 +0530

    Adding personal message feature : <not complete>

--- src/app/core/messages.service.ts
+++ src/app/core/messages.service.ts
@@ -14,9 +14,17 @@ export class MessageService {
     constructor(private af: AngularFire) {
     }
 
-    getMessages(group) {
+    getGroupMessages(group) {
 
         return this.af.database.list('/messages/' + group);
+
+    }
+    
+    getUserMessages(user1, User2) {
+        
+        let databasePathref = (user1.Id > User2.Id: user1.Id + User2.Id ? User2.Id + user1.Id);
+        
+        return this.af.database.list('/messages/' +databasePathref);
 
     }
 
